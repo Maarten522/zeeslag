dd750967048dd8e6f77e673247db0ba73e461722
Maarten Van der Schueren <maarten.vanderschueren@telenet.be>
2025-10-21 20:16:17 +0200

    Created board.java

---
+++ src/main/java/be/ugent/battleship/model/Board.java
@@ -0,0 +1,46 @@
+package be.ugent.battleship.model;
+
+public class Board {
+    private Cell[][] cellen;
+    int r, c, aantalRijen, aantalKolommen;
+
+    public Board(int aantalRijen, int aantalKolommen) {
+        this.aantalRijen = aantalRijen;
+        this.aantalKolommen = aantalKolommen;
+        cellen = new Cell[aantalRijen][aantalKolommen];
+
+        for (r = 0; r < aantalRijen; r++){
+            for (c = 0; c < aantalKolommen; c++){
+                cellen[r][c] = new Cell();
+            }
+        }
+    }
+
+    public Cell getCell(int x, int y){
+        return cellen[x][y];
+    }
+
+    public void plaatsSchepen(int bootLengte, int x, int y, String richting, String soortSchip) {
+        boolean horizontaal = richting.equalsIgnoreCase("horizontal");
+        boolean verticaal = richting.equalsIgnoreCase("vertical");
+
+        for (int i = 0; i < bootLengte; i++) {
+            int startX = x;
+            int startY = y;
+
+            if (horizontaal) {
+                startX = x + i;
+            } else if (verticaal) {
+                startY = y + i;
+            }
+
+            Cell cel = cellen[startX][startY]; // rij = startY, kolom = startX
+            cel.setType("ship");
+            cel.setSoortSchip(soortSchip);
+
+            System.out.printf("Cel geplaatst op (%d,%d), soort %s%n", startX, startY, soortSchip);
+        }
+
+    }
+
+}
